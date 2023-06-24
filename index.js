58fd5bed0e737b5e782c4a393c986ede8215a8af
nsmckay <111510526+nsmckay@users.noreply.github.com>
2023-06-24 19:12:26 +0100

    some initial Javascript to prepopulate input boxes, and also store input vales in variables

--- index.js
+++ index.js
@@ -6,6 +6,37 @@ const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 const tabBtn = document.getElementById("tab-btn")
 
+const countryInput = document.getElementById("country-input")
+const dateInput = document.getElementById("date-input")
+const dateOutput = document.getElementById("date-output")
+
+let today = new Date()
+let todayString = today.toDateString()
+
+let queryCountry = countryInput.value
+let queryDate = today
+let queryDateString = todayString
+
+document.addEventListener('DOMContentLoaded', function() {
+    dateInput.setAttribute("value", todayString)
+    dateOutput.valueAsDate = today
+}, false)
+
+countryInput.addEventListener("change", function() {
+    queryCountry = countryInput.value
+    console.log(queryCountry)
+})
+
+dateInput.addEventListener("change", function() {
+    queryDateString = dateInput.value
+    console.log(queryDateString)
+})
+
+dateOutput.addEventListener("change", function() {
+    queryDate = dateOutput.value
+    console.log(queryDate)
+})
+
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
